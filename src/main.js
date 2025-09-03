0e93a2f56841f61eec3d7aadd438a9a10e65bc40
wu <2194207628@qq.com>
2025-09-03 16:25:46 +0800

    add Element-Plus Module in Project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+const app=createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
